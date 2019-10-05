7a83481adcae53af856b5cf73391c4e545ff19a2
RafaelButler <49237824+RafaelButler@users.noreply.github.com>
2019-10-05 13:35:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 
-mongoose.connect('mongodb+srv://EstudandoNode:EstudandoNode@estudandonode-gobdz.mongodb.net/EstudandoNode?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true}, () => console.log('Connected to DB'));
+mongoose.connect('mongodb+srv://DB_Connect:DB_Connect@estudandonode-gobdz.mongodb.net/DB_Connect?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true}, () => console.log('Connected to DB'));
 app.use(routes);
 
 
